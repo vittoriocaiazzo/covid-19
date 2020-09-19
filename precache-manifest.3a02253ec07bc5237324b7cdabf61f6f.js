4ded8fc396db6cc13af2eedda2e7150af87e7ec2
Vittorio Caiazzo <vitto@Vittorios-MacBook-Pro.local>
2020-09-19 19:05:29 +0200

    Updates

---
+++ precache-manifest.3a02253ec07bc5237324b7cdabf61f6f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7d267c95bf002fdcc16e6ae04337cd",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "68d2e63a71c3a9d97812",
+    "url": "/covid-19/static/css/main.c95f8d60.chunk.css"
+  },
+  {
+    "revision": "2c1faf27939f5dfc7c56",
+    "url": "/covid-19/static/js/2.afbaaafa.chunk.js"
+  },
+  {
+    "revision": "cf4100369c4fb768a91524a34c423768",
+    "url": "/covid-19/static/js/2.afbaaafa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d2e63a71c3a9d97812",
+    "url": "/covid-19/static/js/main.694d5f62.chunk.js"
+  },
+  {
+    "revision": "ca6f7991abbe432f93f7",
+    "url": "/covid-19/static/js/runtime-main.8e18646f.js"
+  },
+  {
+    "revision": "2446d46ce9180fab1c14e82180aec871",
+    "url": "/covid-19/static/media/cancel-circle.2446d46c.svg"
+  },
+  {
+    "revision": "1fc7ff0b941570453667b57847926e4c",
+    "url": "/covid-19/static/media/github.1fc7ff0b.svg"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/covid-19/static/media/hamburger.86595150.svg"
+  },
+  {
+    "revision": "f80a0f2404d47756f6cece2974ac4b4c",
+    "url": "/covid-19/static/media/linkedin.f80a0f24.svg"
+  },
+  {
+    "revision": "bf3113cdf3a30207d9a5399b7aea0f0b",
+    "url": "/covid-19/static/media/search.bf3113cd.svg"
+  },
+  {
+    "revision": "195eca3b1be621a6af28bec74aef267b",
+    "url": "/covid-19/static/media/sort-amount-asc.195eca3b.svg"
+  },
+  {
+    "revision": "b2bc0afcc1882678b089b8a62390d4cb",
+    "url": "/covid-19/static/media/sort-amount-desc.b2bc0afc.svg"
+  },
+  {
+    "revision": "c88c47fbc110e9a4183cd13d816338af",
+    "url": "/covid-19/static/media/stats-bars.c88c47fb.svg"
+  },
+  {
+    "revision": "3b070670c39480c4d18a34f2400fc4d4",
+    "url": "/covid-19/static/media/stats-dots.3b070670.svg"
+  }
+]);
